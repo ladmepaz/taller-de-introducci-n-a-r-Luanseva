23cd698a0aa7f13e6d1dee131858a3d3d2728751
Luanseva <lsena@unal.edu.co>
2024-10-30 09:59:33 -0500

    Update ejercicio01.R

--- ejercicio01.R
+++ ejercicio01.R
@@ -2,8 +2,8 @@
 
 solucion <- function() {
   # Asigna valores a 'a' y 'b' de manera que el valor asignado a 's' sea 100
-  a <- 50 # asigna valor aquí
-  b <- 50 # asigna valor aquí
+  a <- 49 # asigna valor aquí
+  b <- 51 # asigna valor aquí
   s <- a+b
   return(s)
 }
